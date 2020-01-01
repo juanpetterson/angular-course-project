c59ab48718d7772138791072eaa0640821034f76
juanpetterson <juan.petterson@gmail.com>
2020-01-01 20:59:58 -0300

    Adjusted some styles

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, Input, HostListener, HostBinding } from "@angular/core";
+import {
+  Directive,
+  Input,
+  HostListener,
+  HostBinding,
+  ElementRef
+} from "@angular/core";
 
 @Directive({
   selector: "[appDropdown]"
@@ -6,7 +12,10 @@ import { Directive, Input, HostListener, HostBinding } from "@angular/core";
 export class DropdownDirective {
   @HostBinding("class.open") isOpen: boolean = false;
 
-  @HostListener("click") toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener("document:click", ["$event"]) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
   }
+  constructor(private elRef: ElementRef) {}
 }
